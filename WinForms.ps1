61c604b36498a696ac5de70ef86d2e08b602a556
Dinuirar <dinuirar@gmail.com>
2018-12-31 01:03:36 +0100

    winForms app prototype

--- WinForms.ps1
+++ WinForms.ps1
@@ -5,4 +5,26 @@ $main_form.Text = 'PowerShell Commander'
 $main_form.Width = 600
 $main_form.Height = 400
 $main_form.AutoSize = $true
+$gbHeight = $main_form.Height
+
+$groupboxL = New-Object system.Windows.Forms.Groupbox
+$groupboxL.Height = $gbHeight
+$groupboxL.Width = $main_form.Width / 2
+$groupboxL.Text = "pane L"
+$groupboxL.location = New-Object System.Drawing.Point(0,0)
+
+$main_form.controls.AddRange(@($groupboxL,$files))
+$groupboxL.Controls.AddRange(@($files))
+
+$groupboxR = New-Object system.Windows.Forms.Groupbox
+$groupboxR.Height = $gbHeight
+$groupboxR.Width = $main_form.Width / 2
+$groupboxR.Text = "pane R"
+$position = $main_form.Width / 2
+$groupboxR.location = New-Object System.Drawing.Point($position,0)
+
+$main_form.controls.AddRange(@($groupboxR,$files))
+$groupboxR.Controls.AddRange(@($files))
+
 $main_form.ShowDialog()
+
